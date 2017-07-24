77d78ead04dd1b9cebc201fa17073cbc9bee45a2
AlexRimmerQA <alex.rimmer@qa.com>
2017-07-24 16:27:55 +0100

    Added a few html pages, added multiple routes with a static path and a dynamic path

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,4 +9,20 @@ class Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def saywhat(what:String) = Action {
+    if(what == "WHAT") {
+      Ok(views.html.saywhat("You are a cool dude"))
+    } else {
+      Ok(views.html.saywhat("Maaaan, you just don't understand, get out of here"))
+    }
+  }
+
+  def justSomePage() = Action {
+    Ok(views.html.justsomepage())
+  }
+
+  def pageWithStuff() = Action {
+    Ok(views.html.pagewithstuff())
+  }
+
 }
